c62129857232abeb7dcbad5a439d9622b5346766
Brijeshlakkad <brijeshlakkad22@gmail.com>
2021-02-05 20:12:00 +0530

    First commit. Project setup

---
+++ src/main/java/com/warzone/team08/exceptions/InvalidInputException.java
@@ -0,0 +1,11 @@
+package com.warzone.team08.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
