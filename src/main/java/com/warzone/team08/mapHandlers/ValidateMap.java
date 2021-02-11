328b1b6fb48f2fdfaa90d6a360db313ee37c11d7
Deep-P-Patel <pateldeepp301198@gmail.com>
2021-02-11 16:19:30 +0530

    ValidateMap Version 1.0 with IDE auto-formatter

--- src/main/java/com/warzone/team08/mapHandlers/ValidateMap.java
+++ src/main/java/com/warzone/team08/mapHandlers/ValidateMap.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * This class is contains methods for the validation of the map.
+ *
  * @author Deep
  */
 
@@ -22,68 +23,50 @@ public class ValidateMap {
 
     /**
      * This method checks all the validation and reply back to the execute method.
+     *
      * @return String to the execute method.
      */
     public String validation1() {
 
-        if (d_Components.getContinentList().size() > 0)
-        {
-            if (d_Components.getCountryList().size() > 1)
-            {
-                if (d_Components.getCountryList().size() >= d_Components.getContinentList().size())
-                {
+        if (d_Components.getContinentList().size() > 0) {
+            if (d_Components.getCountryList().size() > 1) {
+                if (d_Components.getCountryList().size() >= d_Components.getContinentList().size()) {
                     TreeMap<Integer, List<Integer>> l_NeighbourList = d_Components.getCountryNeighbourMap();
                     int l_TreeMapSize = l_NeighbourList.size();
                     int l_Counter = 0;
-                    for(Map.Entry<Integer, List<Integer>> l_loop : l_NeighbourList.entrySet())
-                    {
-                        if(l_loop.getValue().size()>0)
-                        {
+                    for (Map.Entry<Integer, List<Integer>> l_loop : l_NeighbourList.entrySet()) {
+                        if (l_loop.getValue().size() > 0) {
                             l_Counter++;
                         }
                     }
-                    if(l_Counter == l_TreeMapSize)
-                    {
+                    if (l_Counter == l_TreeMapSize) {
                         int l_Counter1 = 0;
-                        LinkedHashMap<String,List<String>> l_continentCountryMap = d_Components.getContinentCountryMap();
+                        LinkedHashMap<String, List<String>> l_continentCountryMap = d_Components.getContinentCountryMap();
                         int l_continentCountrySize = l_continentCountryMap.size();
-                        for(Map.Entry<String, List<String>> entry:l_continentCountryMap.entrySet())
-                        {
+                        for (Map.Entry<String, List<String>> entry : l_continentCountryMap.entrySet()) {
                             List<String> l_countryList = entry.getValue();
                             int l_countryCount = l_countryList.size();
-                            if(l_countryCount>0)
-                            {
+                            if (l_countryCount > 0) {
                                 l_Counter1++;
                             }
                         }
-                        if(l_continentCountrySize == l_Counter1)
-                        {
+                        if (l_continentCountrySize == l_Counter1) {
                             return "Validation pass";
-                        }
-                        else
-                        {
+                        } else {
                             return "Continent must has 1 country";
                         }
-                    }
-                    else
-                    {
+                    } else {
                         return "Country must have 1 neighbour";
                     }
-                }
-                else
-                {
+                } else {
                     return "Continents must be lesser or equal to the couties";
                 }
-            }
-            else
-            {
+            } else {
                 return "Atleast 1 country required";
             }
-        }
-        else
-        {
-                return "Atleast 1 continent required";
+        } else {
+            return "Atleast 1 continent required";
         }
 
-        }
     }
+}
