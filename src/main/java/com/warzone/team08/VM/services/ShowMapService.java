101d6545645bf19f844a1a6c50b6a360c70f983d
mileshkotadia987 <mileshkumar98@gmail.com>
2021-02-20 17:36:38 +0530

    updated ShowMapService file for sorting the content to be displayed

--- src/main/java/com/warzone/team08/VM/services/ShowMapService.java
+++ src/main/java/com/warzone/team08/VM/services/ShowMapService.java
@@ -16,7 +16,7 @@ import java.util.*;
  * @version 1.0
  */
 public class ShowMapService implements SingleCommand {
-    MapEditorEngine d_mapEditorEngine=null;
+    MapEditorEngine d_mapEditorEngine;
     ContinentRepository d_continentRepository;
     CountryRepository d_countryRepository;
     Set<Continent> d_continentSet;
@@ -38,15 +38,23 @@ public class ShowMapService implements SingleCommand {
      * @return String of all continents information
      */
     public String showContinentCountryContent() {
+
+        //table header
         String[] l_header = {"Continent Name", "Control Value", "Countries"};
-        ArrayList<ArrayList<String>> l_mapContent = new ArrayList<>();
+        List<List<String>> l_mapContent = new ArrayList<>();
+
         for (Map.Entry<String, Set<String>> l_entry : d_continentCountryMap.entrySet()) {
             ArrayList<String> l_continentsList = new ArrayList<>();
             l_continentsList.add(l_entry.getKey());
             try {
+                //fetching continent object using its name
                 Continent l_continent = d_continentRepository.findFirstByContinentName(l_entry.getKey());
                 l_continentsList.add(String.valueOf(l_continent.getContinentControlValue()));
-                String l_continentCountries = String.join(",", l_entry.getValue());
+
+                //for sorting the countries of continent
+                List<String> l_sortedCountryList=new ArrayList<>(l_entry.getValue());
+                Collections.sort(l_sortedCountryList);
+                String l_continentCountries = String.join(",",l_sortedCountryList);
                 l_continentsList.add(l_continentCountries);
                 l_mapContent.add(l_continentsList);
 
@@ -54,15 +62,22 @@ public class ShowMapService implements SingleCommand {
                 p_e.printStackTrace();
             }
         }
+
+        //for sorting the continent table wrt to continent name
+        Collections.sort(l_mapContent, new Comparator<List<String>>() {
+            @Override
+            public int compare(List<String> o1, List<String> o2) {
+                return o1.get(0).compareTo(o2.get(0));
+            }
+        });
         //store continent data in 2d array
-        String l_continentMapMatrix[][] = new String[l_mapContent.size()][];
+        String[][] l_continentMapMatrix = new String[l_mapContent.size()][];
         for (int i = 0; i < l_mapContent.size(); i++) {
-            ArrayList<String> l_singleContinentContent = l_mapContent.get(i);
+            List<String> l_singleContinentContent = l_mapContent.get(i);
             l_continentMapMatrix[i] = l_singleContinentContent.toArray(new String[l_singleContinentContent.size()]);
         }
 
-        String l_continentMapTable = FlipTable.of(l_header, l_continentMapMatrix);
-        return l_continentMapTable;
+        return FlipTable.of(l_header, l_continentMapMatrix);
     }
 
     /**
@@ -71,31 +86,26 @@ public class ShowMapService implements SingleCommand {
      * @return String of country's neighbour information
      */
     public String showNeighbourCountries(){
-        ArrayList<String> l_countryNames = new ArrayList<>();
+        LinkedList<String> l_countryNames = new LinkedList<>();
         String[][] l_neighbourCountryMatrix = new String[d_countrySet.size() + 1][d_countrySet.size() + 1];
-        l_countryNames.add("Countries");
 
         //for adding all country names
         for (Country l_country : d_countrySet) {
             l_countryNames.add(l_country.getCountryName());
         }
         l_neighbourCountryMatrix[0][0]="COUNTRIES";
+        Collections.sort(l_countryNames);
 
-        //for storing country names in first column of matrix
-        for (int l_row = 0; l_row < l_neighbourCountryMatrix.length; l_row++) {
-            if (l_row != 0)
-                l_neighbourCountryMatrix[l_row][0] = l_countryNames.get(l_row);
-        }
-
-        //for storing country names in first row of matrix
-        for (int l_col = 0; l_col < l_neighbourCountryMatrix.length; l_col++) {
-            if (l_col != 0)
-                l_neighbourCountryMatrix[0][l_col] = l_countryNames.get(l_col);
+        //for storing country names in first row and column of matrix
+        for (int l_row = 1; l_row < l_neighbourCountryMatrix.length; l_row++) {
+            String l_name=l_countryNames.pollFirst();
+            l_neighbourCountryMatrix[l_row][0] = l_name;
+            l_neighbourCountryMatrix[0][l_row] = l_name;
         }
 
         //for storing neighbours of each country
         for (int l_row = 1; l_row < l_neighbourCountryMatrix.length; l_row++) {
-            Country l_countryRow = null;
+            Country l_countryRow;
             try {
                 l_countryRow = d_countryRepository.findFirstByCountryName(l_neighbourCountryMatrix[l_row][0]);
                 List<Country> l_countryNeighbourList = l_countryRow.getNeighbourCountries();
@@ -113,13 +123,12 @@ public class ShowMapService implements SingleCommand {
             }
         }
 
-        String[] l_countryCountHeader=new String[d_countrySet.size()+1];
+        String[] l_countryCountHeader=new String[l_neighbourCountryMatrix.length];
         for(int i=0;i<l_countryCountHeader.length;i++){
             l_countryCountHeader[i]="C"+i;
         }
 
-        String l_neighbourCountryTable = FlipTable.of(l_countryCountHeader,l_neighbourCountryMatrix);
-        return l_neighbourCountryTable;
+        return FlipTable.of(l_countryCountHeader,l_neighbourCountryMatrix);
     }
 
     /**
