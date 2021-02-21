44df82dc3e75867d9cbc0bf67b609a63d895b46e
charit <charit@gmail.com>
2021-02-21 17:45:59 +0530

    Player class with required getter-setter

--- src/main/java/com/warzone/team08/VM/entities/Player.java
+++ src/main/java/com/warzone/team08/VM/entities/Player.java
@@ -13,6 +13,116 @@ import java.util.Set;
 public class Player {
     private Set<Country> d_countrySet;
     private List<Order> d_orders = new ArrayList<>();
+    private int d_turnValue;
+    private List<Country> d_assignedCountries;
+    private String d_name;
+    private int d_initialArmies;
+    private int d_reinforcementArmies;
+    private boolean d_canReinforce;
+
+    /**
+     * Getter method for reinforcement armies.
+     *
+     * @return reinforcement armies.
+     */
+    public int getReinforcementArmies() {
+        return d_reinforcementArmies;
+    }
+
+    /**
+     * Setter method to assign reinforce armies.
+     *
+     * @param p_reinforcementArmies reinforcement armies.
+     */
+    public void setReinforcementArmies(int p_reinforcementArmies) {
+        d_reinforcementArmies = p_reinforcementArmies;
+    }
+
+    /**
+     * getter method to check can reinforce.
+     *
+     * @return true, if player can reinforce armies, otherwise false.
+     */
+    public boolean isCanReinforce() {
+        return d_canReinforce;
+    }
+
+    /**
+     * Setter method to assign reinforce value.
+     *
+     * @param p_canReinforce true/false value according to player can reinforcement or not.
+     */
+    public void setCanReinforce(boolean p_canReinforce) {
+        d_canReinforce = p_canReinforce;
+    }
+
+    /**
+     * Getter method for initial armies.
+     * @return initial armies.
+     */
+    public int getInitialArmies() {
+        return d_initialArmies;
+    }
+
+    /**
+     * Setter method for initial armies.
+     * @param p_initialArmies initial armies.
+     */
+    public void setInitialArmies(int p_initialArmies) {
+        d_initialArmies = p_initialArmies;
+    }
+
+    /**
+     * Getter method for player name.
+     * @return player name.
+     */
+    public String getName() {
+        return d_name;
+    }
+
+    /**
+     * Setter method for player name.
+     * @param p_name player name.
+     */
+    public void setName(String p_name) {
+        this.d_name = p_name;
+    }
+
+    /**
+     * getter method to assign countries.
+     *
+     * @return list of assigned countries.
+     */
+    public List<Country> getAssignedCountries() {
+        return d_assignedCountries;
+    }
+
+    /**
+     * setter method to assign countries.
+     *
+     * @param p_assignedCountries list of assigned countries.
+     */
+    public void setAssignedCountries(List<Country> p_assignedCountries) {
+        d_assignedCountries = p_assignedCountries;
+    }
+
+    /**
+     * Gets the turn value of the particular player.
+     *
+     * @return turn value.
+     */
+    public int getTurnValue() {
+        return d_turnValue;
+    }
+
+    /**
+     * Sets the turn value of the particular player.
+     *
+     * @param p_turnValue turn value.
+     */
+    public void setTurnValue(int p_turnValue) {
+        d_turnValue = p_turnValue;
+    }
 
     /**
      * Gets order from the user and stores the order for the player.
---
+++ src/test/java/com/warzone/team08/VM/services/ContinentServiceTest.java
@@ -0,0 +1,46 @@
+package com.warzone.team08.VM.services;
+
+import com.warzone.team08.VM.exceptions.EntityNotFoundException;
+import com.warzone.team08.VM.exceptions.InvalidInputException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * This class tests the add and remove operations on continent.
+ * @author CHARIT
+ */
+public class ContinentServiceTest {
+    private static ContinentService d_continentService;
+
+    /**
+     * This method runs before the test case class runs. This method initializes different objects required to perform test.
+     */
+    @BeforeClass
+    public static void before() {
+        d_continentService = new ContinentService();
+    }
+
+    /**
+     * Tests whether the continent is successfully added or not.
+     * It passes if continent is added.
+     * @throws InvalidInputException throws if input is invalid.
+     */
+    @Test//(expected = InvalidInputException.class)
+    public void testAddContinent() throws InvalidInputException {
+        String l_str = d_continentService.add("Asia", "10");
+        assertEquals("Asia continent added!", l_str);
+    }
+
+    /**
+     * Tests whether the continent is successfully removed or not.
+     * It passes if continent is removed.
+     * @throws EntityNotFoundException throws if continent if continent is not available in list.
+     */
+    @Test//(expected = InvalidInputException.class)
+    public void testRemoveContinent() throws EntityNotFoundException {
+        String l_str = d_continentService.remove("Asia");
+        assertEquals("Asia continent removed!", l_str);
+    }
+}
