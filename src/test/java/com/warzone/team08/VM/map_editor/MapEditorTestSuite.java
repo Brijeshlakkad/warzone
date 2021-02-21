881a4b6e43167505578dff5244e01b37fb1317e2
Brijeshlakkad <brijeshlakkad22@gmail.com>
2021-02-21 22:25:22 +0530

    Continent Service Test case added

--- src/test/java/com/warzone/team08/VM/map_editor/MapEditorTestSuite.java
+++ src/test/java/com/warzone/team08/VM/map_editor/MapEditorTestSuite.java
@@ -1,5 +1,6 @@
 package com.warzone.team08.VM.map_editor;
 
+import com.warzone.team08.VM.map_editor.services.ContinentServiceTest;
 import com.warzone.team08.VM.map_editor.services.EditMapServiceTest;
 import com.warzone.team08.VM.map_editor.services.LoadMapServiceTest;
 import com.warzone.team08.VM.map_editor.services.ShowMapServiceTest;
@@ -16,7 +17,8 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         EditMapServiceTest.class,
         LoadMapServiceTest.class,
-        ShowMapServiceTest.class
+        ShowMapServiceTest.class,
+        ContinentServiceTest.class
 })
 public class MapEditorTestSuite {
     // This class remains empty, it is used only as a holder for the above annotations
---
+++ src/test/java/com/warzone/team08/VM/map_editor/services/ContinentServiceTest.java
@@ -0,0 +1,61 @@
+package com.warzone.team08.VM.map_editor.services;
+
+import com.warzone.team08.VM.exceptions.EntityNotFoundException;
+import com.warzone.team08.VM.exceptions.InvalidInputException;
+import com.warzone.team08.VM.map_editor.MapEditorEngine;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * This class tests the add and remove operations on continent.
+ *
+ * @author CHARIT
+ * @author Brijesh Lakkad
+ */
+public class ContinentServiceTest {
+    private static ContinentService d_continentService;
+
+    /**
+     * Runs before the test case class runs; Initializes different objects required to perform test.
+     */
+    @BeforeClass
+    public static void beforeClass() {
+        d_continentService = new ContinentService();
+    }
+
+    /**
+     * Re-initializes the continent list before test case run.
+     */
+    @Before
+    public void beforeTestCase() {
+        MapEditorEngine.getInstance().initialise();
+    }
+
+    /**
+     * Tests whether the wrong continent value is being shown or not.
+     *
+     * @throws InvalidInputException Throws if country value is not number.
+     */
+    @Test(expected = InvalidInputException.class)
+    public void testWrongContinentValue() throws InvalidInputException {
+        d_continentService.add("Asia", "StringValue");
+    }
+
+    /**
+     * Tests whether the continent is successfully added and removed or not; Passes if continent is removed.
+     *
+     * @throws EntityNotFoundException Throws if continent is not available in list.
+     * @throws InvalidInputException   Throws if country value is not number.
+     */
+    @Test(expected = Test.None.class)
+    public void testAddAndRemoveContinent() throws EntityNotFoundException, InvalidInputException {
+        String l_responseOfAddOp = d_continentService.add("Asia", "10");
+        assertNotNull(l_responseOfAddOp);
+
+        String l_responseOfRemoveOp = d_continentService.remove("Asia");
+        assertNotNull(l_responseOfRemoveOp);
+    }
+}
