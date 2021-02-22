95d037ad5b71a7d852989bb95f931d6a04f5833c
Brijeshlakkad <brijeshlakkad22@gmail.com>
2021-02-22 14:55:26 +0530

    File#retrieve will create a new file if it doesn't exist

--- src/main/java/com/warzone/team08/VM/utils/FileUtil.java
+++ src/main/java/com/warzone/team08/VM/utils/FileUtil.java
@@ -2,6 +2,7 @@ package com.warzone.team08.VM.utils;
 
 import com.warzone.team08.VM.exceptions.InvalidInputException;
 import com.warzone.team08.VM.exceptions.ResourceNotFoundException;
+import com.warzone.team08.VM.exceptions.VMException;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -31,13 +32,16 @@ public class FileUtil {
      *
      * @param p_filePath Value of the path to file.
      * @return Value of File object for the file given with path.
-     * @throws InvalidInputException     Throws if the file does not exist.
-     * @throws ResourceNotFoundException Throws if file not found.
+     * @throws VMException Throws if the file can not be created.
      */
-    public static File retrieveFile(String p_filePath) throws InvalidInputException, ResourceNotFoundException {
+    public static File retrieveFile(String p_filePath) throws VMException {
         File l_file = new File(PathResolverUtil.resolveFilePath(p_filePath));
         String l_fileName = l_file.getName();
-        checkIfFileExists(l_file);
+        try {
+            l_file.createNewFile();
+        } catch (Exception p_exception) {
+            throw new VMException("File can not be created");
+        }
 
         int l_index = l_fileName.lastIndexOf('.');
         if (l_index > 0) {
