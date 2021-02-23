89703182de03e42675e657f19e42be8baef81dfa
charit <charit@gmail.com>
2021-02-23 10:54:20 +0530

    Creation of DistributeCountriesService.java class for handling assigncountries command

--- src/main/java/com/warzone/team08/VM/game_play/services/DistributeCountriesService.java
+++ src/main/java/com/warzone/team08/VM/game_play/services/DistributeCountriesService.java
@@ -1,5 +1,6 @@
 package com.warzone.team08.VM.game_play.services;
 
+import com.warzone.team08.VM.constants.interfaces.SingleCommand;
 import com.warzone.team08.VM.entities.Continent;
 import com.warzone.team08.VM.entities.Country;
 import com.warzone.team08.VM.entities.Player;
@@ -7,9 +8,9 @@ import com.warzone.team08.VM.exceptions.*;
 import com.warzone.team08.VM.map_editor.MapEditorEngine;
 import com.warzone.team08.VM.map_editor.services.EditMapService;
 
+import java.lang.ArithmeticException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,47 +21,59 @@ import static java.lang.Math.floor;
  *
  * @author CHARIT
  */
-public class DistributeCountriesService {
+public class DistributeCountriesService implements SingleCommand {
 
     public List<Player> d_playerList;
     public List<Country> d_countryList;
-    public static List<Continent> D_ContinentList;
+    public static List<Continent> d_ContinentList;
     public MapEditorEngine d_mapEditorEngine;
+    public EditMapService d_edit;
+    public URL d_testFilePath;
 
     /**
      * Parameterized constructor for instantiating required objects.
      */
     public DistributeCountriesService(List<Player> p_playerList) {
+
         d_mapEditorEngine = MapEditorEngine.getInstance();
-        D_ContinentList = d_mapEditorEngine.getContinentList();
+        d_ContinentList = d_mapEditorEngine.getContinentList();
         d_countryList = d_mapEditorEngine.getCountryList();
         // TODO GameEngine.getInstance().getPlayerList();
-        //d_playerList = new ArrayList<>();
         d_playerList = p_playerList;
     }
 
     /**
      * Method to assign countries to different players.
+     *
+     * @throws InvalidInputException  Throws if number of players are zero.
      */
-    public void distributeCountries() {
+    public String distributeCountries() throws InvalidInputException {
         int l_countryCount = d_countryList.size();
         int l_playerCount = d_playerList.size();
+        int l_floorVal;
+        int l_remainder;
+        try {
+            l_floorVal = (int) floor(l_countryCount / l_playerCount);
+            l_remainder = l_countryCount % l_playerCount;
 
-        int l_floorVal = (int) floor(l_countryCount / l_playerCount);
-        int l_remainder = l_countryCount % l_playerCount;
-
-        for (Player l_playerObj : d_playerList) {
-            if (l_remainder > 0) {
-                l_playerObj.setAssignedCountryCount(l_floorVal + l_remainder);
-                l_remainder--;
-            } else {
-                l_playerObj.setAssignedCountryCount(l_floorVal);
+            for (Player l_playerObj : d_playerList) {
+                if (l_remainder > 0) {
+                    l_playerObj.setAssignedCountryCount(l_floorVal + 1);
+                    l_remainder--;
+                } else {
+                    l_playerObj.setAssignedCountryCount(l_floorVal);
+                }
             }
+            for (Player l_player : d_playerList) {
+                int l_playerCountryCount = l_player.getAssignedCountryCount();
+                List<Country> l_assignedCountryList = assignCountry(l_player, l_playerCountryCount);
+                l_player.setAssignedCountries(l_assignedCountryList);
+            }
+            return "Countries are successfully assigned!";
         }
-        for (Player l_player : d_playerList) {
-            int l_playerCountryCount = l_player.getAssignedCountryCount();
-            List<Country> l_assignedCountryList = assignCountry(l_player, l_playerCountryCount);
-            l_player.setAssignedCountries(l_assignedCountryList);
+        catch (ArithmeticException e)
+        {
+            throw new InvalidInputException("Number of players are zero");
         }
     }
 
@@ -73,32 +86,35 @@ public class DistributeCountriesService {
      */
     public List<Country> assignCountry(Player p_player, int p_playerCountryCount) {
         List<Country> l_assignedCountries = new ArrayList<>();
+        List<Country> l_countryLst;
+        List<Country> l_groupOfCountries;
+        int l_playerCountryCount = p_playerCountryCount;
 
         int l_size;
-        int iterateCountryCount = 0;
+        int l_iterateCountryCount = 0;
         do {
-            Country selectedCountry = d_countryList.get(iterateCountryCount);
-            if (selectedCountry.getOwnedBy() != null) {
+            Country selectedCountry = d_countryList.get(l_iterateCountryCount);
+            if (selectedCountry.getOwnedBy() == null) {
                 selectedCountry.setOwnedBy(p_player);
-                List<Country> l_groupOfCountries = findCountryNeighbors(selectedCountry, p_playerCountryCount);
+                l_groupOfCountries = findCountryNeighbors(selectedCountry);
                 l_groupOfCountries.add(0, selectedCountry);
 
                 l_size = l_groupOfCountries.size();
                 if (l_size < p_playerCountryCount) {
-                    p_playerCountryCount = p_playerCountryCount - l_size;
+                    p_playerCountryCount -= l_size;
                     assignOwnerToCountry(p_player, l_groupOfCountries);
                     l_assignedCountries.addAll(l_groupOfCountries);
                 } else {
-                    List<Country> l_countryLst = l_groupOfCountries.subList(0, p_playerCountryCount);
+                    l_countryLst = l_groupOfCountries.subList(0, p_playerCountryCount);
                     assignOwnerToCountry(p_player, l_countryLst);
                     l_assignedCountries.addAll(l_countryLst);
                 }
             }
-            iterateCountryCount++;
-            if (iterateCountryCount == d_countryList.size()) {
+            l_iterateCountryCount++;
+            if (l_iterateCountryCount == d_countryList.size()) {
                 break;
             }
-        } while (l_assignedCountries.size() < p_playerCountryCount);
+        } while (l_assignedCountries.size() < l_playerCountryCount);
         return l_assignedCountries;
     }
 
@@ -117,15 +133,26 @@ public class DistributeCountriesService {
     /**
      * Finds the neighboring country of the given country.
      *
-     * @param p_country            Country object
-     * @param p_playerCountryCount Number of countries can be assigned to player.
-     * @return List of country object.
-     * @throws IllegalStateException Throws if returns an empty list.
+     * @param p_country               Country object
+     * @return                        List of country object.
+     * @throws IllegalStateException  Throws if returns an empty list.
      */
-    public List<Country> findCountryNeighbors(Country p_country, int p_playerCountryCount) throws IllegalStateException {
+    public List<Country> findCountryNeighbors(Country p_country) throws IllegalStateException {
         return p_country.getNeighbourCountries().stream().filter((p_l_country) ->
-                p_l_country.getOwnedBy() != null
+                p_l_country.getOwnedBy() == null
         ).collect(Collectors.toList());
     }
 
+    /**
+     * Calls the distributeCountries() method of the class and returns the result.
+     *
+     * @param p_commandValues         Represents the values passed while running the command.
+     * @return                        Success message if function runs without error, otherwise throws exception.
+     * @throws InvalidInputException  Throws if number of players are zero.
+     * @throws IllegalStateException  Throws if returns an empty list.
+     */
+    @Override
+    public String execute(List<String> p_commandValues) throws InvalidInputException, IllegalStateException {
+        return distributeCountries();
+    }
 }
