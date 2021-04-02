cc66980ae516937e5a0a3ef089bdecfc510e1fcd
charit <charit@gmail.com>
2021-04-02 22:48:07 +0530

    Initial commit for Adapter pattern implementation

---
+++ src/main/java/com/warzone/team08/VM/map_editor/services/EditConquestMapService.java
@@ -0,0 +1,11 @@
+package com.warzone.team08.VM.map_editor.services;
+
+/**
+ * This class act as an Adaptee.
+ * It loads the conquest map into different java objects.
+ *
+ * @author CHARIT
+ */
+
+public class EditConquestMapService {
+}
